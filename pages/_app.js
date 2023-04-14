3daf79ac4a434ed242d7005533f4546acfbf7f98
bijink <kbijin528.pro@gmail.com>
2023-04-14 19:51:01 +0530

    Add head elements
    Add head title & icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 import { ApolloProvider } from "@apollo/client";
 import client from "../apollo-client";
+import Head from "next/head";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <>
+      <Head>
+        <title>Pokémon Catalog</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
   );
 }
